8d85a275c1c18b13b0bf4478fa0cc9d74c4c6825
NateTewolde <tewolde.nate@gmail.com>
2022-12-05 00:55:19 -0800

    Adjust ItemCart to use the adjusted product property .addItem instead of using it as a prop

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,8 +3,7 @@ const ItemCard = ({ product, addItem, removeItem }) => {
     <div className="item-card">
       <p>{product.name + ":" + product.price}</p>
       <div className="item-btns">
-        <button onClick={addItem}>add</button>
-        <button onClick={removeItem}>subtract</button>
+        <button onClick={product.addItem}>Add to Cart</button>
       </div>
     </div>
   );
