6086287571a5c1ee34a9560319ae09f185640476
NateTewolde <tewolde.nate@gmail.com>
2022-12-05 05:24:40 -0800

    Adjust .name property calls to .title to fit updated properties

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,7 +1,15 @@
+const formatCurrency = (price) => {
+  const currencyOptions = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  };
+  return price.toLocaleString(undefined, currencyOptions);
+};
+
 const ItemCard = ({ product, addItem, removeItem }) => {
   return (
     <div className="item-card">
-      <p>{product.name + ":" + product.price}</p>
+      <p>{`${product.title}: ${formatCurrency(product.price)}`}</p>
       <div className="item-btns">
         <button onClick={product.addItem}>Add to Cart</button>
       </div>
