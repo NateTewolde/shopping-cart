1d9eba97e6836f0c61ca9e3756eedeb51379e6fc
NateTewolde <tewolde.nate@gmail.com>
2022-12-08 04:47:45 -0800

    Update shop to take a category parameter and display items matching the category

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 import uniqid from "uniqid";
 import CategoriesBar from "../components/CategoriesBar";
 import Footer from "../components/Footer";
 import ItemCard from "../components/ItemCard";
 
-const Shop = ({ products, categories }) => {
+const getCategoryItems = (products, category) => {
+  return products.filter((product) => product.category === category);
+};
+
+const Shop = ({ products, categories, category }) => {
+  const [shopProducts, setShopProducts] = useState([]);
+
+  useEffect(() => {
+    if (category !== undefined) {
+      setShopProducts(getCategoryItems(products, category));
+    } else {
+      setShopProducts(products.slice(0));
+    }
+  }, [category, products]);
+
   return (
     <div className="shop-wrapper">
       <CategoriesBar categories={categories} />
       <div className="shop-items">
-        {products.map((product) => {
+        {shopProducts.map((product) => {
           return <ItemCard key={uniqid()} product={product} />;
         })}
       </div>
