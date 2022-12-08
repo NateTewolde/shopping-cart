748f9488ef632782d3481c4640b978fa4a79adbb
NateTewolde <tewolde.nate@gmail.com>
2022-12-08 01:13:14 -0800

    Remove Nav component call to call it from App.js instead

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,8 @@
-import Nav from "../components/NavBar";
 import Footer from "../components/Footer";
 
 const About = () => {
   return (
     <div className="about-wrapper">
-      <Nav />
       About
       <Footer />
     </div>
