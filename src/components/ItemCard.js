c013f209576682703523a6e8f8a310e1d22855af
NateTewolde <tewolde.nate@gmail.com>
2022-12-09 04:27:13 -0800

    Add getStars() to accurately get the rating in stars filled in the exact amount of the rating

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,3 +1,5 @@
+import uniqid from "uniqid";
+
 const formatCurrency = (price) => {
   const currencyOptions = {
     minimumFractionDigits: 2,
@@ -6,12 +8,71 @@ const formatCurrency = (price) => {
   return price.toLocaleString(undefined, currencyOptions);
 };
 
+const getStars = (rate) => {
+  let blankStars;
+  const stars = [];
+  for (let i = 0; i <= rate; i++) {
+    let extraGoldPercent = (Math.round((rate - i) * 10) / 10) * 100;
+
+    if (rate - i < 1) {
+      console.log(`extraGoldPercent: ${extraGoldPercent}`);
+
+      blankStars = 4 - stars.length;
+      let background;
+      if (extraGoldPercent > 50) {
+        background = `linear-gradient(to right,  #ffd700, #ffd700 ${extraGoldPercent}%, #333030 ${
+          100 - extraGoldPercent
+        }%, #333030)`;
+      } else {
+        background = `linear-gradient(to left,  #333030,#333030 ${
+          100 - extraGoldPercent
+        }%, #ffd700 ${extraGoldPercent}%, #ffd700)`;
+      }
+      stars.push(
+        <span
+          key={uniqid()}
+          className="stars"
+          style={{
+            background: `${background}`,
+            WebkitBackgroundClip: `text`,
+          }}
+        >
+          &#9733;
+        </span>
+      );
+    } else {
+      stars.push(
+        <span key={uniqid()} className="stars">
+          &#9733;
+        </span>
+      );
+    }
+  }
+
+  for (let j = 0; j < blankStars; j++) {
+    stars.push(
+      <span key={uniqid()} className="stars blank-stars">
+        &#9733;
+      </span>
+    );
+  }
+
+  return stars;
+};
+
 const ItemCard = ({ product }) => {
+  const stars = getStars(product.rating.rate);
+
   return (
     <div className="item-card">
       <div className="item-title">{`${product.title}`}</div>
       <div className="item-price">${formatCurrency(product.price)}</div>
       <div className="item-add-btn">
+        <div className="rating">
+          <div>{stars}</div>
+          <div>{`${product.rating.rate}`}</div>
+          <div>{`${product.rating.count} customer ratings`}</div>
+        </div>
         <button onClick={product.addItem}>Add to Cart</button>
       </div>
     </div>
