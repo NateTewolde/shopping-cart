32572bca996e24c3e19293c3cf37520e2385065a
NateTewolde <tewolde.nate@gmail.com>
2022-12-11 02:52:42 -0800

    Add radio buttons for each filter type to filter shop on click

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,49 @@
 const Filter = ({ filterProducts }) => {
   return (
     <div className="filter">
-      <span>Filter by:</span>
-      <div className="filter-options"></div>
-      <button onClick={filterProducts("price", "descending")}>
-        Price (higher to lower)
-      </button>
+      <span>Filter by</span>
+      <div className="filter-options">
+        <div className="filter-choice">
+          <label htmlFor="price-descending">Price (higher to lower)</label>
+          <input
+            type="radio"
+            name="filter-option"
+            title="price"
+            value="descending"
+            onClick={filterProducts}
+          />
+        </div>
+        <div className="filter-choice">
+          <label htmlFor="price-ascending">Price (lower to higher)</label>
+          <input
+            type="radio"
+            name="filter-option"
+            title="price"
+            value="ascending"
+            onClick={filterProducts}
+          />
+        </div>
+        <div className="filter-choice">
+          <label htmlFor="rate-descending">Rate (higher to lower)</label>
+          <input
+            type="radio"
+            name="filter-option"
+            title="rate"
+            value="descending"
+            onClick={filterProducts}
+          />
+        </div>
+        <div className="filter-choice">
+          <label htmlFor="rate-ascending">Rate (lower to higher)</label>
+          <input
+            type="radio"
+            name="filter-option"
+            title="rate"
+            value="ascending"
+            onClick={filterProducts}
+          />
+        </div>
+      </div>
     </div>
   );
 };
