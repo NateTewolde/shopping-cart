92c419897102a628c5e6932cf2871938b10149b8
NateTewolde <tewolde.nate@gmail.com>
2022-12-14 05:51:49 -0800

    Create component for each cart item

---
+++ src/components/CartItem.js
@@ -0,0 +1,41 @@
+import plusSign from "../assets/images/plus.svg";
+import minusSign from "../assets/images/minus.svg";
+
+const formatCurrency = (price) => {
+  const currencyOptions = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  };
+  return price.toLocaleString(undefined, currencyOptions);
+};
+
+const CartItem = ({ item, handleInputQuantityChange }) => {
+  return (
+    <li className="cart-list-item">
+      <img src={item.image} alt={item.title} />
+      <div className="cart-item-title">{`${item.title}`}</div>
+      <div className="cart-item-price">{`$${formatCurrency(item.price)}`}</div>
+      <div className="cart-list-item-btns">
+        <img
+          src={minusSign}
+          alt="Subtract from cart"
+          onClick={item.removeItem}
+        />
+        <span>
+          <input
+            type="text"
+            defaultValue={item.quantity}
+            onBlur={handleInputQuantityChange}
+            id={item.title}
+          ></input>
+        </span>
+        <img src={plusSign} alt="Add to cart" onClick={item.addItem} />
+      </div>
+      <div className="cart-list-item-total">
+        {`$${formatCurrency(item.sumPrice)}`}
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
