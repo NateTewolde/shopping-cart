def62efedcc4e7e1e518c85940049bfcb512c8be
NateTewolde <tewolde.nate@gmail.com>
2022-12-16 06:04:04 -0800

    Change home link to use a image instead of text and remove About link for easier styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+import stopSign from "../assets/images/stop-sign.svg";
 
 const NavBar = () => {
   return (
     <header>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <div className="home-logo">
+              <img src={stopSign} alt="stop sign" />
+              <div className="logo-text">ONE</div>
+            </div>
+          </Link>
         </li>
         <li>
           <Link to="/shop">Shop</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
         </li>
       </ul>
     </header>
