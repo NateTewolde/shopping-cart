3ba80117cd97607e0833ef5a5b6d05a4681dbf42
NateTewolde <tewolde.nate@gmail.com>
2022-12-23 02:35:41 -0800

    Remove framer motion to slow lagging cart toggle

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,6 @@
 import plusSign from "../assets/images/plus.svg";
 import minusSign from "../assets/images/minus.svg";
 import * as React from "react";
-import { motion } from "framer-motion";
 
 const formatCurrency = (price) => {
   const currencyOptions = {
@@ -11,31 +10,9 @@ const formatCurrency = (price) => {
   return price.toLocaleString(undefined, currencyOptions);
 };
 
-const variants = {
-  open: {
-    delay: 1,
-    opacity: 1,
-    transition: {
-      y: { stiffness: 1000, velocity: -100 },
-    },
-  },
-  closed: {
-    delay: 1,
-    opacity: 0,
-    transition: {
-      y: { stiffness: 1000 },
-    },
-  },
-};
-
 const CartItem = ({ item, handleInputQuantityChange }) => {
   return (
-    <motion.li
-      className="cart-list-item"
-      variants={variants}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.95 }}
-    >
+    <li className="cart-list-item">
       <img src={item.image} alt={item.title} />
       <div className="cart-item-title">{`${item.title}`}</div>
       <div className="cart-item-price">{`$${formatCurrency(item.price)}`}</div>
@@ -58,7 +35,7 @@ const CartItem = ({ item, handleInputQuantityChange }) => {
       <div className="cart-list-item-total">
         {`$${formatCurrency(item.sumPrice)}`}
       </div>
-    </motion.li>
+    </li>
   );
 };
 
