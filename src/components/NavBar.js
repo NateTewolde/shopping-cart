779e93d93a14de239eec8af43c9819fef026c5df
NateTewolde <tewolde.nate@gmail.com>
2022-12-23 06:08:15 -0800

    Replace home logo image with a text logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,17 @@
 import { Link } from "react-router-dom";
-import stopSign from "../assets/images/stop-sign.svg";
 
 const NavBar = () => {
   return (
-    <header>
+    <nav className="navbar">
       <ul>
-        <li>
-          <Link to="/">
-            <div className="home-logo">
-              <img src={stopSign} alt="stop sign" />
-              <div className="logo-text">ONE</div>
-            </div>
-          </Link>
+        <li className="home-logo">
+          <Link to="/">ONE STOP</Link>
         </li>
         <li>
           <Link to="/shop">Shop</Link>
         </li>
       </ul>
-    </header>
+    </nav>
   );
 };
 
