5e1fa1cd19e89ed42fd58319e9497df74f2abb09
NateTewolde <tewolde.nate@gmail.com>
2022-12-26 02:27:00 -0800

    Add handleChange to update the value on <select> when changed

--- src/components/SortBy.js
+++ src/components/SortBy.js
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import uniqid from "uniqid";
 
 const SortBy = ({ sortProducts }) => {
   const sortOptions = [
-    "none",
+    "",
     "Price (higher to lower)",
     "Price (lower to higher)",
     "Rate (higher to lower)",
     "Rate (lower to higher)",
   ];
 
+  const [selectValue, setSelectValue] = useState("");
+
+  const handleChange = (e) => {
+    // if (e.target.value === "") {
+    //   return -1;
+    // }
+    setSelectValue(e.target.value);
+    sortProducts(e);
+  };
+
   return (
     <div className="sort">
       <label htmlFor="sort-options">Sort by: </label>
-      <select id="sort-options" name="sort-options" onChange={sortProducts}>
+      <select
+        value={selectValue}
+        id="sort-options"
+        name="sort-options"
+        onChange={handleChange}
+      >
         {sortOptions.map((sortOption) => (
           <option className="sort-option" value={sortOption} key={uniqid()}>
             {sortOption}
