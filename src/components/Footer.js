58cd96b4b545e06a2be35e7928b21b86224e8aef
NateTewolde <tewolde.nate@gmail.com>
2022-12-27 05:44:36 -0800

    Added copyright and github linked icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,15 @@
+import githubIcon from "../assets/images/github-svgrepo-com.svg";
+
 const Footer = () => {
-  return <footer>footy</footer>;
+  return (
+    <footer>
+      Copyright © 2022 NateTewolde
+      <img
+        href="https://github.com/NateTewolde/shopping-cart"
+        src={githubIcon}
+        alt="Github.com icon"
+      ></img>
+    </footer>
+  );
 };
 export default Footer;
