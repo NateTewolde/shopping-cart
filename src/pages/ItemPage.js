5e9d4ac72cf2239eceede27692f67edce5a0998d
NateTewolde <tewolde.nate@gmail.com>
2022-12-27 05:46:10 -0800

    Add item category and price

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -16,28 +16,31 @@ const ItemPage = ({ product }) => {
   };
 
   return (
-    <div className="item-page-wrapper">
-      <img
-        className="item-page-img"
-        src={product.image}
-        alt={product.description}
-      />
-      <div className="item-page-name">{product.title}</div>
-      <div className="item-page-rating">
-        <StarsRating rate={product.rating.rate} />
-        <div>{`(${product.rating.rate})`}</div>
-        <div className="item-page-rating-count">{`${product.rating.count} customer ratings`}</div>
+    <div className="item-page-wrapper content-wrapper">
+      <div className="item-page-img-container">
+        <img src={product.image} alt={product.description} />
       </div>
-      <div className="item-desc">{product.description}</div>
-      <div className="item-page-add">
-        <input
-          id="item-quantity"
-          onChange={handleChange}
-          type="number"
-          min={1}
-          value={itemQuantity}
-        ></input>
-        <button onClick={addQuantity}>Add to Cart</button>
+
+      <div className="item-page-attributes">
+        <div className="item-page-category">{product.category}</div>
+        <div className="item-page-name">{product.title}</div>
+        <div className="item-page-rating">
+          <StarsRating rate={product.rating.rate} />
+          <div className="item-page-rate">{`(${product.rating.rate})`}</div>
+          <div className="item-page-rating-count">{`${product.rating.count} customer ratings`}</div>
+        </div>
+        <div className="item-desc">{product.description}</div>
+        <div className="item-page-add">
+          <input
+            id="item-quantity"
+            onChange={handleChange}
+            type="number"
+            min={1}
+            value={itemQuantity}
+          ></input>
+          <button onClick={addQuantity}>Add to Cart</button>
+          <div className="item-page-price">{product.price}</div>
+        </div>
       </div>
     </div>
   );
