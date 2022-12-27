a2d4f6afc7f36e7373bf30e3dbb66401f88989d5
NateTewolde <tewolde.nate@gmail.com>
2022-12-27 05:45:20 -0800

    Add a slogan and a button linking to the shop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div className="home-wrapper content-wrapper"></div>;
+  return (
+    <div className="home-wrapper content-wrapper">
+      <div className="home-content">
+        <div className="home-slogan">Your One Stop Shop</div>
+        <div className="home-description">Trendy. Affordable. Convenient.</div>
+        <div className="home-shop-now-button">
+          <button>
+            <Link to="/shop">Shop</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Home;
