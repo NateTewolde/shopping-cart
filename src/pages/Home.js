eba4a6ba147027ebb9e0fb361c86eeaf1a700121
NateTewolde <tewolde.nate@gmail.com>
2023-01-04 03:20:49 -0800

    Adjust Link tag to encompass button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,17 @@ const Home = () => {
         <div className="home-slogan">Your One Stop Shop</div>
         <div className="home-description">Trendy. Affordable. Convenient.</div>
         <div className="home-shop-now-button">
-          <LazyMotion features={domAnimation}>
-            <m.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-              <Link to="/one-stop/shop">Shop</Link>
-            </m.button>
-          </LazyMotion>
+          <Link to="/one-stop/shop">
+            <LazyMotion features={domAnimation}>
+              <m.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                Shop
+              </m.button>
+            </LazyMotion>
+          </Link>
         </div>
       </div>
     </div>
   );
 };
+
 export default Home;
