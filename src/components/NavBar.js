ff53947436eb0189d8a36fbd362bcc4d24207163
NateTewolde <tewolde.nate@gmail.com>
2023-01-04 03:24:19 -0800

    Reorganize to not have the home logo in the ul tag

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar">
+      <div className="home-logo">
+        <Link to="/one-stop">ONE STOP</Link>
+      </div>
       <ul>
-        <li className="home-logo">
-          <Link to="/one-stop">ONE STOP</Link>
-        </li>
         <li>
           <Link to="/one-stop/shop">Shop</Link>
         </li>
